9bd00168ccc73352c57bc8c4b33cd231abebdbca
99IIRUDII66 <78655523+99IIRUDII66@users.noreply.github.com>
2022-02-09 15:42:54 +0300

    Create modulo.c
    Operator modulo (in C)

---
+++ modulo.c
@@ -0,0 +1,21 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+
+    // To store two integer values
+    int x, y, result;
+
+
+
+    x = 0;
+    y = 123;
+    result = x / y;
+    printf("division = %d\n", result);
+
+    result = x % y;
+    printf("modulo = %d\n", result);
+
+
+}
